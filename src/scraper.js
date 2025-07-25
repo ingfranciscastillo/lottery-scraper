fa6af4c3b15440c2cb5930a846073a2711f5a0ba
Francis Miguel Castillo Cruz <franciscastillocruz@gmail.com>
2025-07-25 16:20:32 -0400

    Elimina el tiempo de espera de 30 segundos en la navegación a la página en el scraper.

--- src/scraper.js
+++ src/scraper.js
@@ -35,6 +35,7 @@ export class LotteryScraper {
       console.log("Navegando a la página...");
       await this.page.goto(this.url, {
         waitUntil: "networkidle2",
+        timeout: 0, // Elimina el tiempo de espera de 30 segundos
       });
 
       console.log("Esperando a que carguen los resultados...");
